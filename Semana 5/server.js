3c035cb70da01a5e4a0ef02ab510acce3d0e7d6b
unieimmycortes-byte <unieimmycortes@gmail.com>
2026-06-02 17:06:33 -0500

    server.js

---
+++ Semana 5/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
+
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send(`Hola ${nombre}`);
+});
+
+app.post('/api', (req, res) => {
+  res.json({
+    mensaje: 'Recibido',
+    datos: req.body
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
